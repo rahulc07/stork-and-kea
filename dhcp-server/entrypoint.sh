2b98565ffd8a16cdbe2189ccd9c75b97f2a2de04
Rahul Chandra <rahul@chandra.net>
2026-03-01 22:24:12 -0800

    Remove control agent

--- dhcp-server/entrypoint.sh
+++ dhcp-server/entrypoint.sh
@@ -5,7 +5,6 @@ if [ -z "$(ls -A /etc/kea)" ]; then
     echo "Config directory /etc/kea is empty. Defaulting to basic configs"
     echo "Generating kea-dhcp4.conf from template using INTERFACES=$INTERFACES"
     envsubst < /etc/kea-distribution/kea-dhcp4.conf.template > /etc/kea/kea-dhcp4.conf
-    cp /etc/kea-distribution/kea-ctrl-agent.conf /etc/kea/kea-ctrl-agent.conf
 fi
 chown -R _kea:_kea /etc/kea /run/kea
 chmod -R 750 /run/kea
