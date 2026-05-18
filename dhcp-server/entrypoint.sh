00aa75877780af34d71be7ac8a6036d2d8d53852
Rahul Chandra <rahul@chandra.net>
2026-05-17 20:12:44 -0700

    Implement postgres db for host reservations

--- dhcp-server/entrypoint.sh
+++ dhcp-server/entrypoint.sh
@@ -6,6 +6,25 @@ if [ -z "$(ls -A /etc/kea)" ]; then
     echo "Generating kea-dhcp4.conf from template using INTERFACES=$INTERFACES"
     envsubst < /etc/kea-distribution/kea-dhcp4.conf.template > /etc/kea/kea-dhcp4.conf
 fi
+
+until pg_isready -U "$DB_USER" -h "/var/run/postgresql"; do
+  sleep 1
+done
+
+echo "Checking if 'kea' database exists..."
+DB_EXISTS=$(psql -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='kea'")
+
+if [ "$DB_EXISTS" != "1" ]; then
+    echo "'kea' database not found. creating..."
+    psql -d postgres -c "CREATE DATABASE kea;"
+fi
+
+DB_ARGS="-u $DB_USER -p ${DB_PASSWORD} -n kea -a /var/run/postgresql"
+if ! kea-admin db-check postgresql $DB_ARGS > /dev/null 2>&1; then
+    echo "Tables missing, running migrations..."
+    kea-admin db-init postgresql $DB_ARGS
+fi
+
 chown -R _kea:_kea /etc/kea /run/kea /var/lib/kea
 chmod -R 750 /run/kea
 
